400c2983c5a98f6c6d3559ad13ed673372b45d70
unknown <epeng1994@gmail.com>
2022-06-07 13:59:01 -0400

    completed until 6 GET

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // require your server and launch it here
+const express = require('express')
+//const cors = require('cors')
+const postsRouter = require('./api/posts/posts-router')
+
+const port = 9000
+
+const server = express()
+
+server.use(express.json())
+//server.use(cors())
+
+server.use('/api/posts', postsRouter)
+
+server.listen(port,()=>{
+    console.log(`Server is listening on ${port}`)
+})
